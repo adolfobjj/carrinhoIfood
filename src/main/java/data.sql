ba7e69f82f6e4fc5e7786ad38a76633c7804035e
adolfobjj <luizadolfo7@gmail.com>
2022-09-29 13:23:50 -0300

    doc:Criação do arquivo data.sql

--- src/main/java/data.sql
+++ src/main/java/data.sql
@@ -9,7 +9,7 @@ INSERT INTO cliente(id, cep, complemento, name) VALUES
 INSERT INTO produto(id, disponivel,name, valor_unitario ,restaurante_id) VALUES
 (1L,true, 'X-Java', 15.0, 1L),
 (2L,true, 'X-dotNet', 10.0, 2L),
-(3L,true, 'X-FrontEnd', 18.0, 1L);
+(3L,true, 'X-FrontEnd', 18.0, 3L);
 
 INSERT INTO sacola(id, forma_pagamento, fechada ,valor_total, cliente_id) VALUES
 (1L,0,false,0.0,1L);
