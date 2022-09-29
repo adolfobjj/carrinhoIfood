024e749abf4e406ee08c8e3d6cc7c955af69046c
adolfobjj <luizadolfo7@gmail.com>
2022-09-29 20:16:35 -0300

    feat: criado SacolaService POST E GET

---
+++ src/main/java/com/ifood/carrinhoapi/resource/SacolaResource.java
@@ -0,0 +1,26 @@
+package com.ifood.carrinhoapi.resource;
+
+
+import com.ifood.carrinhoapi.model.Item;
+import com.ifood.carrinhoapi.model.Sacola;
+import com.ifood.carrinhoapi.service.SacolaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/carrinho")
+public class SacolaResource {
+
+    private final SacolaService sacolaService;
+
+    @PostMapping
+    public Item incluirItemNaSacola(@RequestBody  ItemDto itemDto){
+        return sacolaService.incluirItemNaSacola(new ItemDto();
+    }
+    @GetMapping("/{id}")
+    public Sacola verSacola(@PathVariable("id") Long id){
+        return sacolaService.verSacola(id);
+    }
+
+}
