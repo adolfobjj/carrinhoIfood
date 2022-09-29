81d1b8fd536b76c51e410d6af3be80761fa70a82
adolfobjj <luizadolfo7@gmail.com>
2022-09-29 20:15:06 -0300

    feat: criado itemDto

---
+++ src/main/java/com/ifood/carrinhoapi/resource/ItemDto.java
@@ -0,0 +1,20 @@
+package com.ifood.carrinhoapi.resource;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class ItemDto {
+    private Long produtoId;
+    private int quantidade;
+    private Long idSacola;
+
+}
